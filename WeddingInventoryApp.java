bdec1cc111cc65a4413234835ec16eeecc25c7df
Jai_Vang <jaivang0387@gmail.com>
2024-03-15 13:42:48 -0400

    changing main class name

--- WeddingInventoryApp.java
+++ WeddingInventoryApp.java
@@ -2,7 +2,7 @@ import javax.swing.SwingUtilities;
 
 import src.gui_inventory_management.InventoryManagementSystemFrame;
 
-public class MainApp {
+public class WeddingInventoryApp {
     // Main method to launch the application
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
