3b852135941c3284ff0943d8eddcada7b71bd63e
Jai_Vang <jaivang0387@gmail.com>
2024-03-20 16:18:53 -0400

    adding error handing to buttons, moving some codes around so it doesn't print on gui if inventory list is empty

--- src/gui_components/GUIComponentInitializer.java
+++ src/gui_components/GUIComponentInitializer.java
@@ -713,16 +713,18 @@ public class GUIComponentInitializer {
         permanentDeleteButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Update the text area with deleted items
+                // Read from deleted item csv file and set to list
                 List<InventoryItem> deletedItemsList = CSVHandler.readItemsFromDeletedCSVFile();
-                UpdateDeletedItemsTextArea.populateTextArea(deletedItemsList, deletedInventoryTextArea);
 
-                // Check if there are items to delete
+                // Check if there are items to delete, exit method
                 if (deletedItemsList.isEmpty()) {
                     JOptionPane.showMessageDialog(null, "Nothing to Permanently Delete", "Info",
                             JOptionPane.INFORMATION_MESSAGE);
-                    return; // Exit the method
+                    return;
                 }
+
+                // Update the text area with deleted items
+                UpdateDeletedItemsTextArea.populateTextArea(deletedItemsList, deletedInventoryTextArea);
 
                 // Prompt the user to choose between deleting one item, all items, or cancel
                 Object[] options = { "One Item", "All Items", "Cancel" };
@@ -877,15 +879,18 @@ public class GUIComponentInitializer {
         restoreButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Update the text area with deleted items
+                // Read from deleted item csv file and set to list
                 List<InventoryItem> deletedItemsList = CSVHandler.readItemsFromDeletedCSVFile();
-                UpdateDeletedItemsTextArea.populateTextArea(deletedItemsList, deletedInventoryTextArea);
+
                 // Check if there are items to restore
                 if (deletedItemsList.isEmpty()) {
                     JOptionPane.showMessageDialog(null, "No Items to Restore", "Info",
                             JOptionPane.INFORMATION_MESSAGE);
                     return; // Exit the method
                 }
+
+                // Update the text area with deleted items
+                UpdateDeletedItemsTextArea.populateTextArea(deletedItemsList, deletedInventoryTextArea);
 
                 // Prompt the user to choose between restoring one item, all items, or cancel
                 Object[] options = { "Restore One Item", "Restore All Items", "Cancel" };
@@ -1031,6 +1036,13 @@ public class GUIComponentInitializer {
         generateReportButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                // check if the list is empty, exit method
+                if (inventoryItemsList.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "No items to generate report for!", "Information",
+                            JOptionPane.INFORMATION_MESSAGE);
+                    return;
+                }
 
                 // Update inventory text area to display current inventory
                 UpdateInventoryTextArea.reloadTextArea(inventoryItemsList, inventoryTextArea);
